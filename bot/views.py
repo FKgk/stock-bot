2dcd32f8b04a362c026efdae7ec2d489bb596ea6
FKgk <rhkd865@gmail.com>
2020-03-15 00:41:12 +0900

    add models

--- bot/views.py
+++ bot/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from .models import User, Issue
+import json
 
 def set_urls(request):
+	request = json.loads(request)
+	text = request['result']['text']
+	chat_id = request['result']['chat']['id']
+
+	if text.startswith('/start'):
+		msg_start(chat_id)
+
 	pass
 
-def msg_start(request):
+def msg_start(chat_id):
+	result = User.objects.create(id=chat_id)
+	if result:
+		result.save()
+
 
 
 
